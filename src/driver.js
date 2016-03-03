08fc77caa7a4a694b53de69fcb040eecc9d03643
Dag <me@dag.im>
2016-03-03 17:15:14 +0100

    switch to wiring-pi for gpio

--- src/driver.js
+++ src/driver.js
@@ -1,10 +1,11 @@
 import sleep from './sleep';
 import log from './log';
-import * as gpio from "pi-gpio-promise";
+import * as wpi from "wiring-pi";
 
 // BOARD numbering system (not BCM)
 // https://sourceforge.net/p/raspberry-gpio-python/wiki/BasicUsage/
-const pinNumbers = [11, 12, 13, 15];
+// const pinNumbers = [11, 12, 13, 15];
+const pinNumbers = [17, 18, 21, 22];
 
 // rotating x steps using the single step method
 // http://www.lirtex.com/robotiacs/stepper-motor-controller-circuit/
@@ -17,23 +18,34 @@ const steps =
 // acquire pins
 export function setup() {
   log.debug('configuring driver');
-  const promises = pinNumbers.map(n => gpio.open(n, "output"));
-  return Promise.all(promises);
+
+  wpi.setup('gpio');
+
+  pinNumbers.forEach(n => wpi.pinMode(n, wpi.OUTPUT));
+
+  return Promise.resolve();
+  // const promises = pinNumbers.map(n => gpio.open(n, "output"));
+  // return Promise.all(promises);
 }
 
 // release pins
 export function cleanup() {
   log.debug('releasing gpio pins');
-  const promises = pinNumbers.map(n => gpio.close(n));
-  return Promise.all(promises);
+  // const promises = pinNumbers.map(n => gpio.close(n));
+  // return Promise.all(promises);
+  return Promise.resolve();
 }
 
 function set(states) {
     // if(pins == null) {
     //   throw new Error("Pins have not been aquired yet. Please call setup() first.");
     // }
-    const promises = pinNumbers.map((n, i) => gpio.write(n, states[i]));
-    return Promise.all(promises);
+
+    pinNumbers.forEach((n,i) => wpi.digitalWrite(n, states[i]));
+    return Promise.resolve();
+
+    // const promises = pinNumbers.map((n, i) => gpio.write(n, states[i]));
+    // return Promise.all(promises);
 }
 
 export async function drive(counter) {
